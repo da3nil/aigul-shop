5482aae3a0c04e82968e68a1ff298b20f4bcc7a4
danil <d.prytckov@yandex.ru>
2021-05-25 20:08:22 +0500

    add statuses for adding products in cart

--- resources/views/shop/products/show.blade.php
+++ resources/views/shop/products/show.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.user', ['activePage' => 'dashboard', 'titlePage' => __('Dashboard')])
 
 @section('content')
-    <div class="container">
+    <div class="container mt-3">
+
+        @if($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show w-100 mb-3" style="font-size: 1rem" role="alert">
+                <strong>Ошибка!</strong> {{ $errors->first() }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+
+        @if(session('success'))
+            <div class="alert alert-success alert-dismissible fade show w-100 mb-3" style="font-size: 1rem" role="alert">
+                <strong>Успех!</strong> {{ session()->get('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
 
         <div class="titlepb-0 mb-0">
             <h5 class=""><a
