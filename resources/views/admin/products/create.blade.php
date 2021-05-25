9eb7abe3874b61f3f42b5216aae1bcc5df99b8c6
Руслан <ruslan.valeev18117@gmail.com>
2021-05-26 01:29:14 +0500

    6

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -5,22 +5,36 @@
         <div class="container">
             <div class="row d-flex justify-content-center"> 
                 <div class="col-md-8">
+                <form action="{{route('admin.products.store')}}" method="post" enctype="multipart/form-data">
+                    @csrf
                     <div class="input-group mb-3 d-flex ">
                         <span class="input-group-text" id="inputGroup-sizing-default">Название товара</span>
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                        <input name="name" type="text" class="form-control" aria-label="Sizing example input " aria-describedby="inputGroup-sizing-default" >
                     </div>
                     <div class="input-group mb-3">
                       <span class="input-group-text" id="inputGroup-sizing-default">Описание товара</span>
-                      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                      <input name="content" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" >
                     </div>
                     <div class="input-group mb-3">
                       <span class="input-group-text" id="inputGroup-sizing-default">Цена</span>
-                      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                      <input name="price" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" >
                     </div>
                     <div class="input-group mb-3">
                       <span class="input-group-text" id="inputGroup-sizing-default">Изображение</span>
-                      <input type="file" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                      <input name="img" type="file" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                     </div>
+                    <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default" >Категория</span>
+                        <select name="category_id" class="form-control" id="exampleFormControlSelect1">  
+                        @foreach ($categories as $category)
+                        <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach 
+                        </select>
+                    </div>
+                    <div class="text-center">
+                     <button class="btn btn-primory" type="submit">Сохранить</button>
+                     </div>
+                   </form>
                 </div>
             </div>
         </div>            
