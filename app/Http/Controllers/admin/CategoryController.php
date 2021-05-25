4babadec8f3f34cc2fe2ef0673d3c63a0ee00919
danil <d.prytckov@yandex.ru>
2021-05-25 19:35:20 +0500

    admin logic for products and categories

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\admin;
 
 use App\Category;
 use App\Http\Controllers\Controller;
-use App\Product;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,17 +14,19 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        return view('admin.products.index');
+        $categories = Category::all();
+
+        return view('admin.products.index', compact('categories'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function create()
     {
@@ -32,56 +36,94 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Requests\CategoryStoreRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request): RedirectResponse
     {
-        //
+        $data = $request->all();
+
+        $model =(new Category())->fill($data);
+
+        $result = $model->save();
+
+        if (!$result) {
+            return back()->withErrors(['msg' => 'Ошибка сохранения категории']);
+        }
+
+        return redirect()
+            ->route('categories.index')
+            ->with(['success' => 'Категория успешно создана']);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show($id)
+    public function show(int $id)
     {
-        return view('admin.categories.show');
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function edit($id)
+    public function edit(int $id)
     {
-        return view('admin.categories.edit');
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\CategoryUpdateRequest  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(CategoryUpdateRequest $request, int $id): RedirectResponse
     {
+        $data = $request->all();
 
+        $model = Category::findOrFail($id);
+
+        $result = $model->update($data);
+
+        if (!$result) {
+            return back()->withErrors(['msg' => 'Ошибка обновления категории']);
+        }
+
+        return redirect()
+            ->route('categories.index')
+            ->with(['success' => 'Категория успешно обновлена']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy($id)
+    public function destroy(int $id): RedirectResponse
     {
-        //
+        $model = Category::findOrFail($id);
+
+        $result = $model->delete();
+
+        if (!$result) {
+            return back()->withErrors(['msg' => 'Ошибка удаления категории']);
+        }
+
+        return redirect()
+            ->route('categories.index')
+            ->with(['success' => 'Товар успешно удален']);
     }
 }
--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\admin;
 
-use App\Category;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductStoreRequest;
+use App\Http\Requests\ProductUpdateRequest;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -16,19 +17,17 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //$categories = Category::all();
+        $products = Product::with('category')->paginate(20);
 
-        //$products = Product::with('category')->orderByDesc('id')->take(4)->get();
+        $data = compact('products');
 
-        //$data = compact('categories', 'products');
-
-        return view('admin.products.index');
+        return view('admin.products.index', $data);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function create()
     {
@@ -38,56 +37,90 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \App\Http\Requests\ProductStoreRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request): \Illuminate\Http\RedirectResponse
     {
-        //
+        $data = $request->all();
+
+        $model = Product::create($data);
+
+        if (!$model->exists()) {
+            return back()->withErrors(['msg' => 'Ошибка создания товара']);
+        }
+
+        return back()->with(['success' => 'Товар успешно создан']);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show($id)
+    public function show(int $id)
     {
-        return view('admin.products.show');
+        $product = Product::findOrFail($id);
+
+        $data = compact('product');
+
+        return view('admin.products.show', $data);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function edit($id)
+    public function edit(int $id)
     {
-        return view('admin.products.edit');
+        $product = Product::findOrFail($id);
+
+        $data = compact('product');
+
+        return view('admin.products.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\ProductUpdateRequest  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(ProductUpdateRequest $request, int $id): \Illuminate\Http\RedirectResponse
     {
-        //
+        $product = Product::findOrFail($id);
+
+        $data = $request->all();
+
+        if (!$product->update($data)) {
+            return back()->withErrors(['msg' => 'Ошибка обновления товара']);
+        }
+
+        return back()->with(['success' => 'Товар успешно обновлен']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function destroy($id)
+    public function destroy(int $id): \Illuminate\Http\RedirectResponse
     {
-        //
+        $product = Product::findOrFail($id);
+
+        $result = $product->delete();
+
+        if (!$result) {
+            return back()->withErrors(['msg' => 'Ошибка удаления товара']);
+        }
+
+        return redirect()
+            ->route('products.index')
+            ->with(['success' => 'Товар успешно удален']);
     }
 }
