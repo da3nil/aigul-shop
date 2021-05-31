18b1ca9ae192e9fd9d2e4db3dae5401df00ffc34
Руслан <ruslan.valeev18117@gmail.com>
2021-05-31 15:04:07 +0500

    10

--- resources/views/shop/categories/show.blade.php
+++ resources/views/shop/categories/show.blade.php
@@ -18,7 +18,7 @@
                     <div class="row">
                         @foreach($products as $product)
                             <div class="col-md-4 pl-2 pr-2 mb-3">
-                                <a href="{{ route('products.show', ['product' => $product->id]) }}" class="border border-danger card card-chart my-card m-0 h-100">
+                                <a href="{{ route('products.show', ['product' => $product->id]) }}" class="border-magenta card card-chart my-card m-0 h-100">
                                     <div class="card-body">
                                         <div class="text-center">
                                             <img class="img-fluid mb-2" src="{{ asset($product->img) }}" alt="" style="height: 160px">
