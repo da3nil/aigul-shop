40cb386413c0bcd3fd226a906ab41b7a19fee3ac
danil <d.prytckov@yandex.ru>
2021-05-31 09:10:10 +0500

    update about view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
     <div class="container justify-content-center">
-        <h3 class="font-weight-bold mb-4">О компании</h3>
-        <div class="row ">
-
-            <div class="row ">
+        <div class="mb-5">
+            <h3 class="font-weight-bold mb-4">О компании</h3>
+            <div class="row">
                 <div class="col-md-4 p-3">
                     <div class="card h-100">
-                        <img src={{ asset('images/box.png') }} class="card-img-top w-75  m-auto" alt="...">
+                        <img src="{{ asset('images/box.png') }}" class="card-img-top w-75  m-auto" alt="...">
                         <div class="card-body">
                             <h5 class="card-title text-center mb-2">Широкий <br>  ассортимент</h5>
                             <p class="card-text text-center h4">Для юридических лиц доступны товары, находящиеся не только на
@@ -18,7 +17,7 @@
                 </div>
                 <div class="col-md-4 p-3">
                     <div class="card h-100">
-                        <img src={{ asset('images/cart.png') }} class="card-img-top w-75  m-auto" alt="...">
+                        <img src="{{ asset('images/cart.png') }}" class="card-img-top w-75  m-auto" alt="...">
                         <div class="card-body">
                             <h5 class="card-title text-center mb-2" >Товары напрямую от производителя</h5>
                             <p class="card-text text-center fs-3 h4">Мы являемся авторизованным партнером крупнейших производителей
@@ -28,7 +27,7 @@
                 </div>
                 <div class="col-md-4 p-3">
                     <div class="card h-100">
-                        <img src={{ asset('images/war.png') }} class="card-img-top w-75  m-auto" alt="...">
+                        <img src="{{ asset('images/war.png') }}" class="card-img-top w-75  m-auto" alt="...">
                         <div class="card-body">
                             <h5 class="card-title text-center mb-2 ">Удобное сервисное <br> обслуживание</h5>
                             <p class="card-text text-center h4">Мы сотрудничаем со всеми брендами и
