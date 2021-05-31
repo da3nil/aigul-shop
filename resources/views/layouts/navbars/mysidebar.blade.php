dc27a8c9064538dfe5e3dc4d5b9b9fe869097208
Руслан <ruslan.valeev18117@gmail.com>
2021-05-31 11:08:50 +0500

    4

--- resources/views/layouts/navbars/mysidebar.blade.php
+++ resources/views/layouts/navbars/mysidebar.blade.php
@@ -10,10 +10,11 @@
         </div>
     </div>
 
-    <ul class="mb-3 nav my-nav nav-pills nav-pills-icons flex-xl-column pr-0 mr-0" role="tablist">
+    <ul class="mb-3 nav my-nav nav-pills nav-pills-icons flex-xl-column pr-0 mr-0 " role="tablist">
         @foreach($categories as $category)
-            <li class="nav-item">
-                <a class="w-100 btn btn-primary text-white" href="{{ route('categories.show', ['category' => $category->id]) }}" aria-selected="true">
+            <li class="nav-item col-md-12 ">
+                <a class=" w-100  btn  btn-primary h-100 text-white p-2 d-flex align-items-center justify-content-start" href="{{ route('categories.show', ['category' => $category->id]) }}" aria-selected="true">
+                <img class=" rounded float-left h-50 pr-2" style="wight:32px; height:32px;" src={{ asset('images/icons/phone.png') }} alt="">
                     {{ $category->name }}
                 </a>
             </li>
